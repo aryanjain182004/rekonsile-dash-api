ab66ce0d6ac2a299e0c2af52b5433ff77afcc019
Ubuntu <ubuntu@ip-172-31-25-134.eu-north-1.compute.internal>
2024-08-06 04:18:29 +0000

    Add routes for email verification

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,9 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
 import { validationResult } from 'express-validator';
+import axios from 'axios';
+// import speakeasy from "speakeasy";
+const speakeasy = require("speakeasy");
 
 const prisma = new PrismaClient();
 
@@ -16,13 +19,14 @@ const signup = async (req: Request, res: Response) => {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
+    const secret = speakeasy.generateSecret({ length: 20 }).base32;
     const newUser = await prisma.user.create({
       data: {
         firstName,
         lastName,
         email,
         password: hashedPassword,
-        verified: true,  // Marking user as verified
+        totpSecret: secret,
       },
     })
 
@@ -113,5 +117,153 @@ const me = async (req: any, res: Response) => {
   }
 }
 
-export { signup, login, me };
+const sendEmail = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    // Extract data from the request body
+    const { email, firstName } = req.body;
+    const capitalizedName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        totpSecret: true,
+      }
+    })
+    const otp = speakeasy.totp({
+      secret: user?.totpSecret!,
+      encoding: "base32",
+    });
+
+    const msgHTML = `
+    <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+      <div style="margin:50px auto;width:70%;padding:20px 0">
+        <div style="border-bottom:1px solid #eee">
+          <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Rekonsile</a>
+        </div>
+        <p style="font-size:1.1em">Hi, ${capitalizedName}</p>
+        <p>Thank you for choosing Rekonsile. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+        <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+        <p style="font-size:0.9em;">Regards,<br />Rekonsile</p>
+        <hr style="border:none;border-top:1px solid #eee" />
+        <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+          <p>Rekonsile</p>
+          <p>Bangalore</p>
+        </div>
+      </div>
+    </div>`;
+
+    // Send email with OTP
+    await axios.post(
+      `${process.env.NEXT_PUBLIC_OTP_URL}?uname=${
+        process.env.NEXT_PUBLIC_OTP_NAME
+      }&pass=${process.env.NEXT_PUBLIC_OTP_PASSWORD}&fromEmail=${
+        process.env.NEXT_PUBLIC_OTP_FROM_EMAIL
+      }&toEmail=${email}&fromName=Rekonsile&subject=Your%20One-Time%20Password%20(OTP)%20for%20Account%20Verification&msgPlain=Dear ${firstName},&msgHTML=${encodeURIComponent(
+        msgHTML
+      )}`
+    );
+
+    // Send response back to the client
+    res.status(200).json({
+      message: "Email sent successfully",
+    });
+  } catch (error: any) {
+    console.error(error)
+    res.status(500).json({ error: 'Server failed'})
+  }
+};
+
+const verifyOtp = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    // Extract data from the request body
+    const { email, otp } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+      select: {
+        totpSecret: true,
+        firstName: true,
+      },
+    })
+
+    if (user && user.totpSecret) {
+      // Use speakeasy to verify TOTP
+      const valid = speakeasy.totp.verify({
+        secret: user.totpSecret,
+        encoding: "base32",
+        token: otp,
+        window: 60,
+      });
+      if (valid) {
+        await prisma.user.update({
+          where: {
+            email: email,
+          },
+          data: {
+            verified: true,
+          },
+        });
+
+        const formattedMsgHTML = `
+            <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+              <div style="margin:50px auto;width:70%;padding:20px 0">
+                <div style="border-bottom:1px solid #eee">
+                  <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Rekonsile</a>
+                </div>
+                <p style="font-size:1.1em">Hi, ${user?.firstName}</p>
+                <p>Welcome to Reconsile. Hope you are doing well. You have successfully verified your OTP.</p>
+                <p style="font-size:0.9em;">Regards,<br />Rekonsile</p>
+                <hr style="border:none;border-top:1px solid #eee" />
+              <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+              <p>Rekonsile</p>
+              <p>Bangalore</p>
+            </div>
+          </div>
+        </div>`;
+
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_OTP_URL}?uname=${
+            process.env.NEXT_PUBLIC_OTP_NAME
+          }&pass=${process.env.NEXT_PUBLIC_OTP_PASSWORD}&fromEmail=${
+            process.env.NEXT_PUBLIC_OTP_FROM_EMAIL
+          }&toEmail=${email}&fromName=Rekonsile&subject=Welcome%20to%20Rekonsile&msgPlain=Dear Rekonsile,&msgHTML=${encodeURIComponent(
+            formattedMsgHTML
+          )}`
+        );
+
+        res.status(200).json({
+          status: "OTP is valid",
+        })
+      } else {
+        res.status(400).json({
+          status: "OTP is invalid",
+        })
+      }
+
+    } else {
+      res.status(400).json({
+        status: "error while getting user",
+      })
+    }
+  } catch (error: any) {
+    res.status(500).json({ error: 'Server failed'})
+  }
+};
+
+
+
+export { signup, login, me, sendEmail, verifyOtp };
 
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { signup, login, me } from '../controllers/authController';
+import { signup, login, me, sendEmail, verifyOtp } from '../controllers/authController';
 import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
@@ -15,6 +15,10 @@ router.post('/register', [
 router.post('/login', login);
 
 router.get('/me', authMiddleware, me)
+
+router.post('/sendEmail', sendEmail)
+
+router.post('/verifyOtp', verifyOtp)
 
 export default router;
 
