0161ef9a1201fb75ee4612f2f6bd1775377361f7
Ubuntu <ubuntu@ip-172-31-25-134.eu-north-1.compute.internal>
2024-08-07 13:43:52 +0000

    Change redirect url and make currency symbol dynamic

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY
 export const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET
 export const SHOPIFY_SCOPES = process.env.SHOPIFY_SCOPES
-export const CALLBACK_URL = "https://rekonsile.yardstick.live/api/shopify/callback"
+export const CALLBACK_URL = "https://rekonsile-dev.yardstick.live/api/shopify/callback"
 export const FRONTEND_URL = "http://localhost:8083"
